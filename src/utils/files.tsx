82c677582046c90fe4086b181b85bd2444e6d55b
Safal Raj Pandey <safal.pandey.sp+1@gmail.com>
2021-09-05 20:05:07 +0545

    chore: Add util to get file format label
    Also, use enum for support file types

--- src/utils/files.tsx
+++ src/utils/files.tsx
@@ -2,10 +2,22 @@ const GB_TO_BYTES_RATIO = 1073741824;
 const MB_TO_BYTES_RATIO = 1048576;
 const KB_TO_BYTES_RAIO = 1024;
 
-const validExtensions = [
-  `application/vnd.openxmlformats-officedocument.wordprocessingml.document`,
-  `application/msword`,
-  `application/pdf`,
+enum ValidFileTypes {
+  PDF = `application/pdf`,
+  DOC = `application/msword`,
+  DOCX = `application/vnd.openxmlformats-officedocument.wordprocessingml.document`,
+}
+
+const EXTENSION_TO_LABEL_MAP = Object.freeze({
+  [ValidFileTypes.PDF]: 'pdf',
+  [ValidFileTypes.DOC]: 'doc',
+  [ValidFileTypes.DOCX]: 'docx',
+});
+
+const validExtensions: string[] = [
+  ValidFileTypes.PDF,
+  ValidFileTypes.DOC,
+  ValidFileTypes.DOCX,
 ];
 
 export const isValidFileType = (file: File) =>
@@ -24,3 +36,6 @@ export const formatFileSize = (bytes: number): string => {
   }
   return `${bytes.toFixed(2)} B`;
 };
+
+export const getFileFormatLabel = (fileType: string) =>
+  EXTENSION_TO_LABEL_MAP[fileType as ValidFileTypes];
